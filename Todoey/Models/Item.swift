0cf3cc945cbcaaef351812e40351e4a98242be2c
Deepak <deepakrout@hotmail.com>
2020-06-26 08:54:28 -0400

    Intial changes

---
+++ Todoey/Models/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Deepak Rout on 6/25/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable  {
+    var title: String = ""
+    var done: Bool = false
+}
